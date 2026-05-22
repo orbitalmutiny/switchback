d19d6c0d4210eb76b196723a613190a17ad366a6
orbitalmutiny <176943648+orbitalmutiny@users.noreply.github.com>
2026-05-21 20:13:17 -0400

    Restore minimized overlay stats

--- app/src/main/java/com/spop/poverlay/overlay/composables/OverlayMinimizedContent.kt
+++ app/src/main/java/com/spop/poverlay/overlay/composables/OverlayMinimizedContent.kt
@@ -35,7 +35,10 @@ fun OverlayMinimizedContent(
     powerLabel: String,
     cadenceLabel: String,
     speedLabel: String,
+    distanceLabel: String,
     resistanceLabel: String,
+    heartRateLabel: String,
+    caloriesLabel: String,
     isHorizontal: Boolean,
     contentAlpha: Float,
     timerLabel: String,
@@ -136,8 +139,26 @@ fun OverlayMinimizedContent(
             Spacer(modifier = if (isHorizontal) Modifier.width(4.dp) else Modifier.height(4.dp))
             OverlayTimerField(
                 modifier = Modifier.width(58.dp),
+                timerLabel = distanceLabel,
+                iconDrawable = R.drawable.ic_distance
+            )
+            Spacer(modifier = if (isHorizontal) Modifier.width(4.dp) else Modifier.height(4.dp))
+            OverlayTimerField(
+                modifier = Modifier.width(58.dp),
                 timerLabel = resistanceLabel,
                 iconDrawable = R.drawable.ic_resistance
+            )
+            Spacer(modifier = if (isHorizontal) Modifier.width(4.dp) else Modifier.height(4.dp))
+            OverlayTimerField(
+                modifier = Modifier.width(58.dp),
+                timerLabel = heartRateLabel,
+                iconDrawable = R.drawable.ic_heart_rate
+            )
+            Spacer(modifier = if (isHorizontal) Modifier.width(4.dp) else Modifier.height(4.dp))
+            OverlayTimerField(
+                modifier = Modifier.width(58.dp),
+                timerLabel = caloriesLabel,
+                iconDrawable = R.drawable.ic_calories
             )
         }
     }
